5a13170f969bed1afb26caf9c319a42af8ab0c9c
kadde92 <kadir.wafin@gmail.com>
2022-05-05 18:22:03 +0300

    The grid is done and is resizes accordingly

--- app.js
+++ app.js
@@ -3,16 +3,43 @@ const container = document.querySelector('.grid')
 const content = document.createElement('div');
 
 let i = 0;
-let gridLimit = 16*16
+let gridLimit = 20*20
+
 
 
 while (i < gridLimit) {
     const singularDiv = document.createElement('div');
     singularDiv.classList.add('singulardiv');
-    // content.style.cssText = 'border: 1px solid black; background: pink;'
+    // content.style.cssText = `width: ${600/20}; height: ${600/20}`
     container.appendChild(singularDiv)
     i++;
 }
 
-const div = document.querySelectorAll('.singulardiv')
-div.textContent = "hehe"
+// add an eventlistener on mouseover for the divs
+
+const divs = document.querySelectorAll('.singulardiv')
+
+divs.forEach(div => { div.addEventListener('mousemove', blackColor) });
+
+
+function blackColor(e) {
+    // e.target.innerText = 'black'
+    e.target.style.backgroundColor = 'black'
+}
+
+// 1st button = erase
+
+const eraser = document.querySelector('#paska')
+eraser.addEventListener('click', erase)
+
+function erase(e) {
+    divs.forEach(div => { div.addEventListener('mousemove', whiteColor) });
+
+}
+
+function whiteColor(e) {
+    e.target.style.backgroundColor = 'white'
+}
+
+// 2nd button
+
